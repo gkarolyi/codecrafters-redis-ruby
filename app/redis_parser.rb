b165b8936688f3c9ecf00c452afb17ab534d2b60
gkarolyi <33122882+gkarolyi@users.noreply.github.com>
2024-03-04 19:23:04 +1100

    tests for redis encode and decode

--- app/redis_parser.rb
+++ app/redis_parser.rb
@@ -1,15 +1,26 @@
 module RedisParser
-  def self.parse(input)
-    return unless input
+  def self.decode(input)
+    return if input.empty?
 
     resp_array = input.split("\r\n")
     chunks = resp_array[1..].each_slice(2).to_a
-    command = chunks.first[1]
+    command = chunks.first[1].to_sym
     args = chunks[1..].map { |chunk| chunk[1] }
 
     {
-      command: command.to_sym,
+      command:,
       args:
     }
+  end
+
+  def self.encode(input)
+    return if input.empty?
+
+    str_array = input.split(' ')
+    commands = str_array.map { |str| ["$#{str.length}", str.downcase] }.flatten
+
+    ["*#{str_array.length}", commands]
+      .join("\r\n")
+      .concat("\r\n")
   end
 end
--- test/test_redis_parser.rb
+++ test/test_redis_parser.rb
@@ -2,28 +2,48 @@ require 'minitest/autorun'
 require_relative '../app/redis_parser'
 
 class TestRedisParser < Minitest::Test
-  def test_ping
-    assert_equal({ command: :ping, args: [] }, RedisParser.parse("*1\r\n$4\r\nping\r\n"))
+  def test_decode_ping
+    assert_equal({ command: :ping, args: [] }, RedisParser.decode("*1\r\n$4\r\nping\r\n"))
   end
 
-  def test_echo
+  def test_decode_echo
     assert_equal(
       { command: :echo, args: ['bingoasdfasd'] },
-      RedisParser.parse("*2\r\n$4\r\necho\r\n$12\r\nbingoasdfasd\r\n")
+      RedisParser.decode("*2\r\n$4\r\necho\r\n$12\r\nbingoasdfasd\r\n")
     )
   end
 
-  def test_set
+  def test_decode_set
     assert_equal(
       { command: :set, args: %w[z 123124] },
-      RedisParser.parse("*3\r\n$3\r\nset\r\n$1\r\nz\r\n$6\r\n123124\r\n")
+      RedisParser.decode("*3\r\n$3\r\nset\r\n$1\r\nz\r\n$6\r\n123124\r\n")
     )
   end
 
-  def test_get
+  def test_decode_get
     assert_equal(
       { command: :get, args: ['z'] },
-      RedisParser.parse("*2\r\n$3\r\nget\r\n$1\r\nz\r\n")
+      RedisParser.decode("*2\r\n$3\r\nget\r\n$1\r\nz\r\n")
     )
+  end
+
+  def test_decode_blank
+    assert_nil RedisParser.decode('')
+  end
+
+  def test_encode_ping
+    assert_equal "*1\r\n$4\r\nping\r\n", RedisParser.encode('PING')
+  end
+
+  def test_encode_echo
+    assert_equal "*2\r\n$4\r\necho\r\n$12\r\nbingoasdfasd\r\n", RedisParser.encode('ECHO bingoasdfasd')
+  end
+
+  def test_encode_set
+    assert_equal "*3\r\n$3\r\nset\r\n$1\r\nz\r\n$6\r\n123124\r\n", RedisParser.encode('SET z 123124')
+  end
+
+  def test_encode_get
+    assert_equal "*2\r\n$3\r\nget\r\n$1\r\nz\r\n", RedisParser.encode('GET z')
   end
 end
